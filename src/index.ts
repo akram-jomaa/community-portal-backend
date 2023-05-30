c98b4fdb30a43d7773d5a59b9991ebe194cc46a2
Savith Jayasekera <savithcj@icloud.com>
2023-05-30 01:18:14 -0600

    Register cookie plugin

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import {OrganizationHandler} from './handlers/organization'
+import cookie, { FastifyCookieOptions } from "@fastify/cookie";
 import {PrayerTypeHandler} from './handlers/prayerType'
 import {PrayerCallHandler} from './handlers/prayerCall'
 import {PrayerTimeHandler} from './handlers/prayerTime'
@@ -12,6 +13,11 @@ server.register(cors, {
 })
 
 server.get('/api/v1/organization/:id', OrganizationHandler.getByIdHandler)
+server.register(cookie, {
+  secret: process.env.COOKIE_SECRET_KEY as string,
+  hook: false,
+  parseOptions: {} ,
+} as FastifyCookieOptions)
 server.post('/api/v1/organization', OrganizationHandler.createHandler)
 
 server.get('/api/v1/prayer-type', PrayerTypeHandler.getAllHandler)
